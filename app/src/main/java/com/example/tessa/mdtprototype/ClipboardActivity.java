8d766d6d6b15b031439b446f036356c46c251077
Thai <tnguy187@jhu.edu>
2017-04-15 20:20:20 -0400

    revising viewflipper

--- app/src/main/java/com/example/tessa/mdtprototype/ClipboardActivity.java
+++ app/src/main/java/com/example/tessa/mdtprototype/ClipboardActivity.java
@@ -1,15 +1,18 @@
 package com.example.tessa.mdtprototype;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +23,9 @@ public class ClipboardActivity extends SymptomActivity {
     private ArrayList<String> dataArrayList;
     private ArrayAdapter<String> arrayAdapter;
     private RelativeLayout rl;
+    ViewFlipper viewFlipper;
+    private int initial_count=0;
+
 
 
     @Override
@@ -27,12 +33,14 @@ public class ClipboardActivity extends SymptomActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clipboard);
 
-        clipboardListView = (ListView) findViewById(R.id.clipboardListView);
-        dataArrayList = new ArrayList<String>();
-        rl = (RelativeLayout) findViewById(R.id.activity_clipboard);
-
-        writeSummary();
-
+        //clipboardListView = (ListView) findViewById(R.id.clipboardListView);
+        //dataArrayList = new ArrayList<String>();
+        //rl = (RelativeLayout) findViewById(R.id.activity_clipboard);
+        viewFlipper = (ViewFlipper) this.findViewById(R.id.viewFlipper);
+        //writeSummary();
+        //for (int counter = 1;counter<=4;counter++) {
+//        displayView();//
+        //}
     }
 
     public void clearSymptomHistory(View v) {
@@ -41,10 +49,10 @@ public class ClipboardActivity extends SymptomActivity {
     }
 
     public void writeSummary() {
-        dataArrayList = super.getSymptomCount();
+        dataArrayList = super.getSymptomCount(); // access data
         //    String mystring = super.getSymptomCount();
         //    dataArrayList.add(mystring);
-        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataArrayList);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataArrayList);//
         clipboardListView.setAdapter(arrayAdapter);
 
 //        rl.removeAllViews();
@@ -69,6 +77,33 @@ public class ClipboardActivity extends SymptomActivity {
 //        }
 
     }
+
+    public void displayView () {
+        LayoutInflater inflater = (LayoutInflater) this //creates layout inflator
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.calendar_layout, null); //creates new view by inflating testlayout
+        for (int i = 1;i<=4;i++) {
+            viewFlipper.addView(view); //adds inflated view to viewflipper
+            TextView heart_rate = (TextView) view.findViewById(R.id.heart_rate_variable);
+            //ArrayList<String> calendar_list = super.getSymptomCount();
+            //heart_rate.setText(calendar_list.get(i));
+            heart_rate.setText(String.format("%d",initial_count));
+            initial_count++;
+        }
+    }
+
+    public void onButtonDown (View v){
+        viewFlipper.setInAnimation(this, R.anim.low_to_high_enter);
+        viewFlipper.setOutAnimation(this,R.anim.low_to_high_out);
+        viewFlipper.showNext();
+    }
+
+    public void onButtonUp (View v){
+        viewFlipper.setInAnimation(this, R.anim.high_to_low_enter);
+        viewFlipper.setOutAnimation(this,R.anim.high_to_low_out);
+        viewFlipper.showPrevious();
+    }
+
 
     public void backToMenu(View v) {
         Intent openMainActivity= new Intent(ClipboardActivity.this, MainActivity.class);
